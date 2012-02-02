4a6c63ff20ee7a99e328cb70b675bc99ce1a9810
Ryan <snargledorf@gmail.com>
2012-02-02 11:06:54 -0500

    Added the GNU public license to all scripts

--- sdk/sdk-installer.sh
+++ sdk/sdk-installer.sh
@@ -1,6 +1,21 @@
 #!/bin/bash
 #
-# Android SDK Installer Script
+# Android SDK Installer Script: A script used to install the Android SDK, Eclipse, and Open JDK/JRE
+#
+# Copyright (C) 2012  Ryan Esteves
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 # Alot of this script was referenced from https://gist.github.com/1026610
 
